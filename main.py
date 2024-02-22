7422e3da841dd2e43930106048523065232f2e3d
Hugo Antunes <hugo.antunesgoncalves94@gmail.com>
2024-02-22 18:40:43 +0100

    Ajout de nouvelle valeurs

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import os 
+import os
+
+
 def Tracer(x, y, xLabel, yLabel, title):
     try:
         for keys in x.keys():
             plt.plot(
                 x[keys],
                 y[keys],
-                label=f"Tension Vds = {keys}",
+                label=f"Tension Vgs = {keys}V",
             )
         plt.xlabel(f"{xLabel}")
         plt.ylabel(f"{yLabel}")
         plt.title(f"{title}")
+        plt.legend()
         plt.grid()
         # La partie qui suit enregistre les courbes dans un dossier Graphs à la racine du projet
-        if  not os.path.exists("Graphs"):
+        if not os.path.exists("Graphs"):
             os.makedirs("Graphs")
         plt.savefig(f"./Graphs/{title}.png", format="Png")
         plt.show()
@@ -25,59 +28,47 @@ def Tracer(x, y, xLabel, yLabel, title):
 def main():
     # Les clées des dictionnaires sont les tensions Vgs sur lesquelles on travaille
     Vds = {
-        "5": [
-            0,
-            0.2,
-            0.4,
-            0.6,
-            0.8,
-            1.0,
-            1.2,
-            1.4,
-            1.6,
-            1.8,
-            2.0,
-            2.2,
-            2.4,
-            2.6,
-            2.8,
-        ],
-        "10": [
-            0,
-            0.2,
-            0.4,
-            0.6,
-            0.8,
-            1.0,
-            1.2,
-            1.4,
-            1.6,
-            1.8,
-            2.0,
-            2.2,
-            2.4,
-            2.6,
-            2.8,
-        ],
-        "15": [
-            0,
-            0.2,
-            0.4,
-            0.6,
-            0.8,
-            1.0,
-            1.2,
-            1.4,
-            1.6,
-            1.8,
-            2.0,
-            2.2,
-            2.4,
-            2.6,
-            2.8,
-        ],
+        "2": [0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 2.6, 2.8],
+        "3": [0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 2.6, 2.8],
+        "4": [0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 2.6, 2.8],
+        "5": [0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 2.6, 2.8],
     }
     I = {
+        "2": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        "3": [
+            0,
+            0.198,
+            0.353,
+            0.465,
+            0.532,
+            0.555,
+            0.557,
+            0.558,
+            0.559,
+            0.560,
+            0.561,
+            0.562,
+            0.563,
+            0.564,
+            0.565,
+        ],
+        "4": [
+            0,
+            0.419,
+            0.795,
+            1.13,
+            1.42,
+            1.67,
+            1.87,
+            2.03,
+            2.15,
+            2.22,
+            2.24,
+            2.25,
+            2.25,
+            2.26,
+            2.26,
+        ],
         "5": [
             0,
             0.639,
@@ -95,48 +86,12 @@ def main():
             4.99,
             5.0,
         ],
-        "10": [
-            0,
-            1.74,
-            3.44,
-            5.0,
-            # A partir de ces valeurs Tinckercad sature la tension
-            5.0,
-            5.0,
-            5.0,
-            5.0,
-            5.0,
-            5.0,
-            5.0,
-            5.0,
-            5.0,
-            5.0,
-            5.0,
-        ],
-        "15": [
-            0,
-            2.84,
-            # A partir de ces valeurs Tinckercad sature la tension
-            5.0,
-            5.0,
-            5.0,
-            5.0,
-            5.0,
-            5.0,
-            5.0,
-            5.0,
-            5.0,
-            5.0,
-            5.0,
-            5.0,
-            5.0,
-        ],
     }
     # Check si les listes de valeurs ont les mêmes dimension
     for keys in Vds.keys():
-        print(f"{len(Vds[keys])} points for Vds={keys}")
+        print(f"{len(Vds[keys])} points pour Vds={keys}")
     for keys in I.keys():
-        print(f"{len(I[keys])} points for I={keys}")
+        print(f"{len(I[keys])} points pour I={keys}")
     # On trace les courbes nécessaires
     Tracer(
         Vds,
